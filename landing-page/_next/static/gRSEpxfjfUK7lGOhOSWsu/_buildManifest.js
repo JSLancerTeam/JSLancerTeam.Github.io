6c80eb408545bca68d9f61eb7bb442747f87942e
davidtran <nam.trankhanh.vn@gmail.com>
2021-11-23 17:49:28 +0700

    update landing page

--- landing-page/_next/static/gRSEpxfjfUK7lGOhOSWsu/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/384-be399e281c717f3e7d68.js","static/chunks/pages/index-cc070b0c13ea7cbfd88a.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
